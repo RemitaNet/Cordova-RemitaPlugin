c268aef724594fcd48d5f2f13f9745b26a861986
Uchenna Okpagu <uokpagu@chams.com>
2015-11-03 10:25:45 +0100

    first commit

---
+++ remita.js
@@ -0,0 +1,60 @@
+﻿
+/*
+Remita JS Plugin
+2nd November, 2015
+Created by System Specs
+*/
+ 
+    var RemitaController = {         
+
+        LaunchApp: function (url, callback)
+        { 
+
+            var remita_ref = window.open(url, '_blank', 'location=no');
+            remita_ref.addEventListener('loadstart', function (event) {
+                
+
+
+
+                spinnerplugin.show();
+               
+            });
+            remita_ref.addEventListener('loadstop', function (event) {
+                
+
+
+                spinnerplugin.hide();
+
+                setInterval(function () {
+
+                    remita_ref.executeScript({
+                        code: "BillerCtrl.CordovaHandler();"
+                    },
+                            function (values) {
+                                 
+                                if (values[0] !== null)
+                                {
+                                    callback(values[0]);
+
+                                    remita_ref.close();
+                                }
+                                
+                            });
+
+
+
+                }, 1000);
+
+                
+
+                  });
+            remita_ref.addEventListener('loaderror', function (event) {   });
+            remita_ref.addEventListener('exit', function (event)  {
+            });
+
+
+            
+        } 
+
+}
+ 
