fc6599243aeda0aa3dbffc237529b86f53fb51d2
Uchenna Okpagu <uokpagu@chams.com>
2015-12-03 20:53:27 +0100

    Changes to remita.js

--- remita.js
+++ remita.js
@@ -4,61 +4,171 @@ Remita JS Plugin
 2nd November, 2015
 Created by System Specs
 */
+ 
+    var RemitaController = {         
 
-var RemitaController = {
+        LaunchApp: function (url, callback)
+        { 
 
-    LaunchApp: function (url, callback) {
-
-        var remita_ref = window.open(url, '_blank', 'location=no,clearcache=yes,clearsessioncache=yes,zoom=no');
-        remita_ref.addEventListener('loadstart', function (event) {
-
-            spinnerplugin.show();
-
-        });
-        remita_ref.addEventListener('loadstop', function (event) {
-
-
-
-            setTimeout(function () {
-                spinnerplugin.hide();
-
-            }, 6000);
+            var remita_ref = window.open(url, '_blank', 'location=no,clearcache=yes,clearsessioncache=yes,zoom=no');
+            remita_ref.addEventListener('loadstart', function (event) {
+                 
+                spinnerplugin.show();
+               
+            });
+            remita_ref.addEventListener('loadstop', function (event) {
+                
 
 
-            setInterval(function () {
+                setTimeout(function () {
+                    spinnerplugin.hide();
 
+                },6000);
 
+               
+                setInterval(function () {
 
-                remita_ref.executeScript({
-                    code: "BillerCtrl.CordovaHandler();"
-                },
-                        function (values) {
+                   
 
-                            if (values[0] !== null) {
-                                callback(values[0]);
-                                remita_ref.close();
-                                remita_ref.removeEventListener('loadstop', function () {
+                    remita_ref.executeScript({
+                        code: "BillerCtrl.CordovaHandler();"
+                    },
+                            function (valuesx) {
+                                 
+                             
+                                if (valuesx[0] !== null)
+                                {
+                                    callback(valuesx[0]);
+                                    remita_ref.close();
+                                    
+                                    remita_ref.removeEventListener('loadstop', function () {                                     
 
-                                });
-
-
-                            }
-
-                        });
-
-
-
-            }, 2000);
-
-
-
-        });
-        remita_ref.addEventListener('loaderror', function (event) { spinnerplugin.hide(); alert("Unable to connect. Check your connection."); remita_ref.close(); });
-        remita_ref.addEventListener('exit', function (event) {
-        });
+                                    });
+                                   
+                                    
+                                }
+                                
+                            });
 
 
 
-    }
+                }, 2000);
 
+                
+
+                  });
+            remita_ref.addEventListener('loaderror', function (event) { spinnerplugin.hide(); alert("Unable to connect. Check your connection."); remita_ref.close(); });
+            remita_ref.addEventListener('exit', function (event)  {
+            });
+
+
+            
+        },
+        
+        LaunchAppNonFIa: function (url, callback) {
+
+            var remita_ref = window.open(url, '_blank', 'location=no,clearcache=yes,clearsessioncache=yes,zoom=no');
+
+            remita_ref.addEventListener('loadstart', function (event) {
+
+                spinnerplugin.show();
+
+            });
+            remita_ref.addEventListener('loadstop', function (event) {
+                 
+                setTimeout(function () {
+                    spinnerplugin.hide();
+
+                }, 6000);
+                 
+                setInterval(function () {
+ 
+                    remita_ref.executeScript({
+                        code: "CordovaHandler();"
+                    },
+                            function (valuesx) {
+
+
+                                if (valuesx[0] !== null) {
+                                    callback(valuesx[0]);
+                                    remita_ref.close();
+
+                                    remita_ref.removeEventListener('loadstop', function () {
+
+                                    });
+
+
+                                }
+
+                            });
+
+
+
+                }, 2000);
+
+
+            });
+            remita_ref.addEventListener('loaderror', function (event) { spinnerplugin.hide(); alert("Unable to connect. Check your connection."); remita_ref.close(); });
+            remita_ref.addEventListener('exit', function (event) {
+            });
+
+
+
+        },
+
+        LaunchAppNonFIb: function (BillerId, BillId, Name, Phone, Email, Amount, url, callback) {
+
+            var remita_ref = window.open(url + "?BillerId=" + BillerId + "&BillId=" + BillId + "&Name=" + Name + "&Phone=" + Phone + "&Email=" + Email + "&Amount=" + Amount , '_blank', 'location=no,clearcache=yes,clearsessioncache=yes,zoom=no');
+
+            remita_ref.addEventListener('loadstart', function (event) {
+
+                spinnerplugin.show();
+
+            });
+            remita_ref.addEventListener('loadstop', function (event) {
+
+
+
+                setTimeout(function () {
+                    spinnerplugin.hide();
+
+                }, 6000);
+
+
+                setInterval(function () {
+
+
+
+                    remita_ref.executeScript({
+                        code: "CordovaHandler();"
+                    },
+                            function (valuesx) {
+
+
+                                if (valuesx[0] !== null) {
+                                    callback(valuesx[0]);
+                                    remita_ref.close();
+
+                                    remita_ref.removeEventListener('loadstop', function () {
+
+                                    });
+
+
+                                }
+
+                            });
+
+
+
+                }, 2000);
+
+
+
+            });
+            remita_ref.addEventListener('loaderror', function (event) { spinnerplugin.hide(); alert("Unable to connect. Check your connection."); remita_ref.close(); });
+            remita_ref.addEventListener('exit', function (event) {}); 
+
+        }
+         
 }
+ 
